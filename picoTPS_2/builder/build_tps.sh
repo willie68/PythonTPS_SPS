7182ffb9abce475abe2cc9b0739d0f596978aee0
Wilfried Klaas <w.klaas@gmx.de>
2022-11-03 17:08:18 +0100

    trying to use CircuitPython

---
+++ picoTPS_2/builder/build_tps.sh
@@ -0,0 +1,21 @@
+git clone https://github.com/adafruit/circuitpython.git
+cd circuitpython
+
+make fetch-submodules
+
+git checkout main
+
+apt install python3-pip
+
+pip3 install --upgrade -r requirements-dev.txt
+pip3 install --upgrade -r requirements-doc.txt
+
+pre-commit install
+
+make -C mpy-cross
+
+cd ./ports/rasberrypi
+
+copy ./main.py ./modules/main.py 
+
+make BOARD=raspberry_pi_pico
